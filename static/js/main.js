01655db4171500325bf0ed512f3c8bab0c078ff3
despair1 <eveguide0@gmail.com>
2018-03-08 19:54:30 +0500

    insert blank product
    Signed-off-by: despair1 <eveguide0@gmail.com>

--- static/js/main.js
+++ static/js/main.js
@@ -8,7 +8,22 @@ function delete_blank(product_id){
                 console.log($(this))
             })
     }
-
+_.templateSettings = {
+  interpolate: /\{\{(.+?)\}\}/g
+};
+// temp vars: product_id, product_name
+var blank_template = _.template(
+'<div class="row" data-product-id-row="{{product_id}}">' +
+    '<div class="col-lg-9">' +
+        '<div data-product-id="{{product_id}}"' +
+            'onClick=\'$("#post_product_id").val($(this).attr("data-product-id"));' +
+            'document.forms["invisible_form"].submit(); return false;\'>{{product_name}} ' +
+        '</div>' +
+    '</div>' +
+    '<div class="col-lg-2" onclick="delete_blank({{product_id}})">' +
+        'Delete' +
+    '</div>' +
+'</div>' )
 $(document).ready(  function() {
 
     var item;
@@ -30,7 +45,12 @@ $(document).ready(  function() {
             log( "Selected: " + item.value + " aka " + item.label );
         }
     }
-
+    function insert_blank_product(data,status,xhr) {
+        if ( data.status == "blank") {
+            $(blank_template({product_id : item.value,
+                            product_name: item.label})).prependTo("#log")
+        }
+    }
 
 
     $( "#birds" ).autocomplete({
@@ -39,7 +59,7 @@ $(document).ready(  function() {
       select: function( event, ui ) {
         item = ui.item;
         $.getJSON("new_product.json", {
-        product_id: ui.item.value}, success
+        product_id: ui.item.value}, insert_blank_product
         )
         event.preventDefault()
         $("#birds").val("")
